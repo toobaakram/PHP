822138080ff787a6b9352886b272d1adfdc065bb
Tooba Akram <41504736+toobaakram@users.noreply.github.com>
2019-05-15 14:46:43 -0700

    Operators of php

---
+++ Operators.php
@@ -0,0 +1,33 @@
+<?php
+#variable 
+
+$name = " Tooba ";
+$name1 ='Akram';
+echo "My first name is $name";
+echo "<br>";
+echo "My last name is $name1";
+
+# for single code echo
+echo "<br>";
+echo "<br>";
+$name = 'Tooba ';
+$name1 ='Akram';
+echo 'My first name is $name';
+echo "<br>";
+echo 'My last name is $name1';
+
+# for this use Contintation
+
+echo "<br>";
+echo "<br>";
+$name = 'Tooba ';
+$name1 ='Akram';
+echo 'My first name is '. $name;
+echo "<br>";
+echo 'My last name is '.$name1;
+
+#function
+define('TOOBA',"my name is tooba Akram");
+echo "<br>";
+echo TOOBA;
+?>
